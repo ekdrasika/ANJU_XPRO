f28eaaf0d9102f2977171b825b23c6000da12dcd
ekdrasika <ekdrasikarasika@gmail.com>
2025-05-06 10:15:47 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,7 +40,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~bZsEnCgY#bfbrchnzOH9vNzipjEiKaOqoq9W7WAMvtzdn9WJ2g_o",
+    SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~OFtWjAhD#xyaPgFl8D5aznndKsQO24f0G9_6Xjy4otIQZ69NrWV8",
     GITHUB_TOKEN: process.env.GITHUB_TOKEN || "sRuinBjb5k5tkKnGnxfijq9x2XwfgU29vBY1",
     GITHUB_USERNAME: process.env.GITHUB_USERNAME || "ekdrasika",
     SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94717775628", "94758775628"],
